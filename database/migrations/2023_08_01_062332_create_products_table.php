b590bfa2fd3fcbbf0e6a4c9331cab4dcf9ce356b
Sumantsh <sumantsh0@gmail.com>
2023-08-01 12:01:10 +0530

    initial commit

---
+++ database/migrations/2023_08_01_062332_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("prd_name");
+            $table->string("prd_dis");
+            $table->string("prd_image");
+            $table->decimal("prd_min_price");
+            $table->decimal("prd_max_price");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
